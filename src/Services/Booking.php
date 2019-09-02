c8c9989562c402ff906fcf2d68d835bdb1e407e8
Florent Drousset <florent.drousset@gmail.com>
2019-09-02 17:19:50 +0200

    service booking vide

---
+++ src/Services/Booking.php
@@ -0,0 +1,15 @@
+<?php
+
+
+namespace App\Services;
+
+
+use App\Entity\Book;
+use App\Repository\BookRepository;
+
+class Booking
+{
+    public function reservation(BookRepository $br){
+
+    }
+}
