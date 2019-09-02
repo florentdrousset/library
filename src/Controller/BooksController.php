f1e0a987869508ea3a23fa1410afc7bbd764bec5
Florent Drousset <florent.drousset@gmail.com>
2019-09-02 17:06:49 +0200

    add: pages books et book

--- src/Controller/BooksController.php
+++ src/Controller/BooksController.php
@@ -2,6 +2,8 @@
 
 
 namespace App\Controller;
+use App\Entity\Book;
+use App\Repository\BookRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -10,7 +12,18 @@ class BooksController extends AbstractController
     /**
      * @Route("/books", name="books")
      */
-    public function index() {
-        $this->render('books.html.twig');
+    public function index(BookRepository $book) {
+        return $this->render('books/books.html.twig', [
+            'books' => $book->findAll()
+        ]);
+    }
+
+    /**
+     * @Route("/book/{id}", name="book")
+     */
+    public function oneBook(Book $book) {
+        return $this->render('books/book.html.twig', [
+            'book' => $book
+        ]);
     }
 }
