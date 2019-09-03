1ce6e7df39bead37efc3453fa72f513ba2364e3c
Florent Drousset <florent.drousset@gmail.com>
2019-09-03 15:15:49 +0200

    searchUser WIP

--- src/Controller/LibrarianController.php
+++ src/Controller/LibrarianController.php
@@ -8,6 +8,7 @@ use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Entity\User;
 
@@ -21,8 +22,16 @@ class LibrarianController extends AbstractController
         return $this->render(
             'admin/librarian.html.twig',
             [
-                'user' => $this->getUser()
+                'user' => $this->getUser(),
+                'users' => $ur->findAll()
             ]
         );
+    }
+    /**
+     * @Route("/searchUser", name="searchUser")
+     */
+    public function searchUser(Request $request, UserRepository $ur) {
+        $result = $request->request->all();
+        $ur->findBy();
     }
 }
