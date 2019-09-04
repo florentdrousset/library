e4629d5a2a919c47320277463ce5e995563d206e
Lewis <mauconduit.lewis@hotmail.fr>
2019-09-04 17:14:55 +0200

    ajax name

---
+++ public/js/ajaxUpdateUser.js
@@ -0,0 +1,17 @@
+/*
+let input = document.querySelector('#firstName');
+let usersList = document.querySelector('#usersList');
+
+function asyncRequest() {
+    let $value = document.querySelector('#firstName').value;
+    console.log($value);
+    fetch(`autocomplete/` + $value)
+        .then(function(response) {
+            return response.json();
+        }).then(function(data){
+        
+    })
+}
+
+input.addEventListener('keypress', asyncRequest, true);
+*/
