b318f4ae9424cf8c78c5d8e58c4edc252292f41c
Florent Drousset <florent.drousset@gmail.com>
2019-09-05 17:10:27 +0200

    tests

---
+++ tests/Controller/BooksControllerTest.php
@@ -0,0 +1,14 @@
+<?php
+
+
+namespace App\Tests\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+
+class BooksControllerTest extends WebTestCase
+{
+    public function testShowPost() {
+
+    }
+}
